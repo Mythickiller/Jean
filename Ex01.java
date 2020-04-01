800ef79a0bbe1e3d6bfe05e526a0c1e8975f711b
Mythickiller <62970941+Mythickiller@users.noreply.github.com>
2020-03-31 21:23:04 -0300

    Lista 01 java

---
+++ Ex01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ex01 {
+public static void main(String[] args) {
+
+Scanner ler = new Scanner (System.in);
+
+System.out.println("Digite o primeiro número: ");
+int B = ler.nextInt();
+
+System.out.println("Digite o segundo número: ");
+int A = ler.nextInt(); 
+
+
+
+
+System.out.println ("Resultado: " + (A + B));
+
+}
+}
